0730337b810f685f78f63dc2d570206e1b915793
Student User <student@workstation.lab.example.com>
2020-11-25 03:17:54 -0500

    nn

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,4 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "A change is coming. COVID-19\n; WFH until July 2021\n"
+print "A change is coming. COVID-19\n; WFH until July 2021\n "
 ?>
