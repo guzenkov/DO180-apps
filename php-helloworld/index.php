16ad916bcd769bb646adb798ac8a3f8e45d44781
Student User <student@workstation.lab.example.com>
2020-12-02 22:38:35 -0500

    semicolumn was missing

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,5 +1,5 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "A change is coming. COVID-19\n; WFH until July 2021\n "
+print "A change is coming. COVID-19\n; WFH until July 2021\n ";
 print "This s the twentieth DO180 run since list year"
 ?>
