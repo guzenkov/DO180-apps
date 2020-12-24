a11550bc577a5cd72356841db8b6ed1b90b43a2a
Student User <student@workstation.lab.example.com>
2020-12-23 22:56:06 -0500

    Merry Christmas1

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,5 +1,5 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "Happy Thursday everyone!\n";
+print "Happy Thursday everyone! \n \n";
 print "A change is in the air!\n";
 ?>
