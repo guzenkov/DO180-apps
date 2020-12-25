5061ed0fb24d5e750a238a5d94b092582716a9f8
Student User <student@workstation.lab.example.com>
2020-12-24 21:46:19 -0500

    changed index page contents again. This is the last class of the year.

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,5 +1,6 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
 print "A change is coming. COVID-19\n; WFH until July 2021\n ";
-print "This s the twenty-first DO180 run since last year"
+print "This s the twenty-first DO180 run since last year\n";
+print "You are RHU'slast class of 2020. Everyone else is on holidays already\n";
 ?>
